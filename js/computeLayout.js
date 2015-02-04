ff977d5ba3b0f5a65d34921eddbf11630adf6883
curran <curran.kelleher@gmail.com>
2015-02-04 13:38:05 -0800

    Completed computeLayout algorithm and tests, except handlind hidden components

--- js/computeLayout.js
+++ js/computeLayout.js
@@ -1,11 +1,12 @@
 // This module provides a single function that computes a nested box layout.
+//
 // Created by Cuyrran Kelleher Feb 2015
 define(["lodash"], function (_) {
 
-  // Takes as input
+  // Takes as input the following arguments:
   //
   // * `layout` A tree data structure defining nested boxes. The root
-  //   may be a leaf node or internal node.
+  //   of the tree may be either a leaf node or an internal node.
   //   * Leaf nodes are component alias strings.
   //   * Internal nodes are objects with the following properties:
   //     * `orientation` A string, either
@@ -13,19 +14,20 @@ define(["lodash"], function (_) {
   //         with children placed from left to right, or
   //       * "vertical", meaning this node is subdivided vertically
   //         with children placed from top to bottom.
-  //     * `children` - an array of child node objects, which may be leaf
-  //       nodes or internal nodes.
-  // * `sizes` An object where
-  //   * Keys are component aliases
-  //   * Values are objects with the following properties
+  //     * `children` An array of child node objects, each of which may be 
+  //       either leaf nodes or internal nodes.
+  //     * `size` The size of the internal node, with the same specifications
+  //       as values within `sizes` (see next bullet point).
+  // * `sizes` An object that specifies component size options, where
+  //   * Keys are component alias strings.
+  //   * Values are objects with the following properties:
   //     * `size` the width (if the containing box is horizontal)
-  //       or height (if the containing box is vertical) of the component.
-  //       The `size` value may be either:
-  //       * a number (like "1.5" or "1") that determines size relative to 
-  //         siblings within the containing box, or
-  //       * a count of pixels (like "50px" or "200px") that determines an absolute
-  //         fixed size. This is useful in cases where components have fixed-size UI 
-  //         widgets rather than resizable visualizations.
+  //       or height (if the containing box is vertical) of the component. May be either:
+  //       * a number (like "1.5" or "1", expressed as a number or a string) that determines 
+  //         size relative to siblings within the containing box, or
+  //       * a count of pixels (like "50px" or "200px" expressed as a string with "px" suffix)
+  //         that determines an absolute fixed size. This is useful in cases where components 
+  //         have fixed-size UI widgets rather than flexibly resizable visualizations.
   //       * If `size` is not specified, it is assigned a default value of 1.
   //     * `hidden` A boolean for hiding components. If true, the component
   //       is excluded from the layout, if false the component is included.
@@ -35,9 +37,9 @@ define(["lodash"], function (_) {
   //   * `x` The X offset of the box in pixels. If not specified, this defaults to zero.
   //   * `y` The Y offset of the box in pixels. If not specified, this defaults to zero.
   //
-  // The returned result is an object where
+  // Returns an object where
   //
-  //  * Keys are component aliases
+  //  * Keys are component aliases.
   //  * Values are objects representing the computed box for the component, having the following properties:
   //   * `x` The X offset of the box in pixels.
   //   * `y` The Y offset of the box in pixels.
@@ -57,17 +59,21 @@ define(["lodash"], function (_) {
     sizes = sizes || {};
 
     function size(layout){
+      var result;
       if(isLeafNode(layout)){
         alias = layout;
         if((alias in sizes) && ("size" in sizes[alias])){
-          return sizes[alias].size;
+          result = sizes[alias].size;
         } else {
-          return 1;
+          result = 1;
         }
       } else {
-        // TODO test this path
-        return layout.size || 1;
+        result = layout.size || 1;
       }
+      if(typeof result === "string" && ! isPixelCount(result)){
+        result = parseFloat(result);
+      }
+      return result;
     }
 
     if(isLeafNode(layout)){
@@ -86,27 +92,30 @@ define(["lodash"], function (_) {
       y = box.y;
       layout.children.forEach(function (child) {
         var childBox = { x: x, y: y},
+            childSize = size(child),
             sizeInPixels;
-        if(isPixelCount(size(child))){
-          sizeInPixels = pixelCount(size(child));
+
+        if(isPixelCount(childSize)){
+          sizeInPixels = pixelCount(childSize);
         } else {
-          sizeInPixels = (size(child) / sizeSum) * wiggleRoom;
+          sizeInPixels = (childSize / sizeSum) * wiggleRoom;
         }
+
         if(isHorizontal){
           childBox.width = sizeInPixels;
           childBox.height = box.height;
           x += childBox.width;
         } else {
-          // TODO test this path
           childBox.width = box.width;
           childBox.height = sizeInPixels;
           y += childBox.height;
         }
+
         if(isLeafNode(child)){
           alias = child;
           result[alias] = childBox;
         } else {
-          // TODO test this path
+          _.extend(result, computeLayout(child, sizes, childBox));
         }
       });
     }
--- tests/computeLayoutTest.js
+++ tests/computeLayoutTest.js
@@ -83,6 +83,29 @@ describe("computeLayout", function () {
     expect(result.bar.y).to.equal(0);
   });
 
+  it("two components horizontal, relative size as a string", function() {
+    var layout = {
+          orientation: "horizontal",
+          children: ["foo", "bar"]
+        },
+        sizes = {
+          foo: { size: "3" },
+          // bar size defaults to 1
+        },
+        box = { width: 100, height: 100 },
+        result = computeLayout(layout, sizes, box);
+
+    expect(result.foo.width).to.equal(75);
+    expect(result.foo.height).to.equal(100);
+    expect(result.foo.x).to.equal(0);
+    expect(result.foo.y).to.equal(0);
+
+    expect(result.bar.width).to.equal(25);
+    expect(result.bar.height).to.equal(100);
+    expect(result.bar.x).to.equal(75);
+    expect(result.bar.y).to.equal(0);
+  });
+
   it("two components vertical, relative size", function() {
     var layout = {
           orientation: "vertical",
@@ -127,5 +150,240 @@ describe("computeLayout", function () {
     expect(result.bar.height).to.equal(100);
     expect(result.bar.x).to.equal(60);
     expect(result.bar.y).to.equal(0);
+  });
+
+  it("two components vertical, absolute size", function() {
+    var layout = {
+          orientation: "vertical",
+          children: ["foo", "bar"]
+        },
+        sizes = {
+          foo: { size: "60px" },
+          // bar size defaults to 1
+        },
+        box = { width: 100, height: 100 },
+        result = computeLayout(layout, sizes, box);
+
+    expect(result.foo.width).to.equal(100);
+    expect(result.foo.height).to.equal(60);
+    expect(result.foo.x).to.equal(0);
+    expect(result.foo.y).to.equal(0);
+
+    expect(result.bar.width).to.equal(100);
+    expect(result.bar.height).to.equal(40);
+    expect(result.bar.x).to.equal(0);
+    expect(result.bar.y).to.equal(60);
+  });
+
+  it("three components horizontal", function() {
+    var layout = {
+          orientation: "horizontal",
+          children: ["foo", "bar", "baz"]
+        },
+        sizes = {
+          foo: { size: 2},
+        },
+        box = { width: 100, height: 100 },
+        result = computeLayout(layout, sizes, box);
+
+    expect(result.foo.width).to.equal(50);
+    expect(result.foo.height).to.equal(100);
+    expect(result.foo.x).to.equal(0);
+    expect(result.foo.y).to.equal(0);
+
+    expect(result.bar.width).to.equal(25);
+    expect(result.bar.height).to.equal(100);
+    expect(result.bar.x).to.equal(50);
+    expect(result.bar.y).to.equal(0);
+
+    expect(result.baz.width).to.equal(25);
+    expect(result.baz.height).to.equal(100);
+    expect(result.baz.x).to.equal(75);
+    expect(result.baz.y).to.equal(0);
+  });
+
+  it("three components nested horizontal, unspecified sizes", function() {
+    var layout = {
+          orientation: "horizontal",
+          children: [
+            "foo",
+            {
+              orientation: "horizontal",
+              children: ["bar", "baz"]
+            }
+          ]
+        },
+        box = { width: 100, height: 100 },
+        result = computeLayout(layout, null, box);
+
+    expect(result.foo.width).to.equal(50);
+    expect(result.foo.height).to.equal(100);
+    expect(result.foo.x).to.equal(0);
+    expect(result.foo.y).to.equal(0);
+
+    expect(result.bar.width).to.equal(25);
+    expect(result.bar.height).to.equal(100);
+    expect(result.bar.x).to.equal(50);
+    expect(result.bar.y).to.equal(0);
+
+    expect(result.baz.width).to.equal(25);
+    expect(result.baz.height).to.equal(100);
+    expect(result.baz.x).to.equal(75);
+    expect(result.baz.y).to.equal(0);
+  });
+
+  it("three components nested horizontal & vertical, unspecified sizes", function() {
+    var layout = {
+          orientation: "horizontal",
+          children: [
+            "foo",
+            {
+              orientation: "vertical",
+              children: ["bar", "baz"]
+            }
+          ]
+        },
+        box = { width: 100, height: 100 },
+        result = computeLayout(layout, null, box);
+
+    expect(result.foo.width).to.equal(50);
+    expect(result.foo.height).to.equal(100);
+    expect(result.foo.x).to.equal(0);
+    expect(result.foo.y).to.equal(0);
+
+    expect(result.bar.width).to.equal(50);
+    expect(result.bar.height).to.equal(50);
+    expect(result.bar.x).to.equal(50);
+    expect(result.bar.y).to.equal(0);
+
+    expect(result.baz.width).to.equal(50);
+    expect(result.baz.height).to.equal(50);
+    expect(result.baz.x).to.equal(50);
+    expect(result.baz.y).to.equal(50);
+  });
+
+  it("three components nested horizontal & vertical, relative size in layout", function() {
+    var layout = {
+          orientation: "horizontal",
+          children: [
+            "foo",
+            {
+              orientation: "vertical",
+              size: 3,
+              children: ["bar", "baz"]
+            }
+          ]
+        },
+        box = { width: 100, height: 100 },
+        result = computeLayout(layout, null, box);
+
+    expect(result.foo.width).to.equal(25);
+    expect(result.foo.height).to.equal(100);
+    expect(result.foo.x).to.equal(0);
+    expect(result.foo.y).to.equal(0);
+
+    expect(result.bar.width).to.equal(75);
+    expect(result.bar.height).to.equal(50);
+    expect(result.bar.x).to.equal(25);
+    expect(result.bar.y).to.equal(0);
+
+    expect(result.baz.width).to.equal(75);
+    expect(result.baz.height).to.equal(50);
+    expect(result.baz.x).to.equal(25);
+    expect(result.baz.y).to.equal(50);
+  });
+
+  it("three components nested horizontal & vertical, relative size in layout as a string", function() {
+    var layout = {
+          orientation: "horizontal",
+          children: [
+            "foo",
+            {
+              orientation: "vertical",
+              size: "3",
+              children: ["bar", "baz"]
+            }
+          ]
+        },
+        box = { width: 100, height: 100 },
+        result = computeLayout(layout, null, box);
+
+    expect(result.foo.width).to.equal(25);
+    expect(result.foo.height).to.equal(100);
+    expect(result.foo.x).to.equal(0);
+    expect(result.foo.y).to.equal(0);
+
+    expect(result.bar.width).to.equal(75);
+    expect(result.bar.height).to.equal(50);
+    expect(result.bar.x).to.equal(25);
+    expect(result.bar.y).to.equal(0);
+
+    expect(result.baz.width).to.equal(75);
+    expect(result.baz.height).to.equal(50);
+    expect(result.baz.x).to.equal(25);
+    expect(result.baz.y).to.equal(50);
+  });
+
+  it("three components nested horizontal & vertical, absolute size in layout", function() {
+    var layout = {
+          orientation: "horizontal",
+          children: [
+            "foo",
+            {
+              orientation: "vertical",
+              size: "60px",
+              children: ["bar", "baz"]
+            }
+          ]
+        },
+        box = { width: 100, height: 100 },
+        result = computeLayout(layout, null, box);
+
+    expect(result.foo.width).to.equal(40);
+    expect(result.foo.height).to.equal(100);
+    expect(result.foo.x).to.equal(0);
+    expect(result.foo.y).to.equal(0);
+
+    expect(result.bar.width).to.equal(60);
+    expect(result.bar.height).to.equal(50);
+    expect(result.bar.x).to.equal(40);
+    expect(result.bar.y).to.equal(0);
+
+    expect(result.baz.width).to.equal(60);
+    expect(result.baz.height).to.equal(50);
+    expect(result.baz.x).to.equal(40);
+    expect(result.baz.y).to.equal(50);
+  });
+
+  it("three components, absolute size in layout, relative size in sizes", function() {
+    var layout = {
+          orientation: "horizontal",
+          children: [
+            "foo",
+            {
+              orientation: "vertical",
+              size: "60px",
+              children: ["bar", "baz"]
+            }
+          ]
+        },
+        sizes = { bar: { size: 3 }},
+        box = { width: 100, height: 100 },
+        result = computeLayout(layout, sizes, box);
+
+    expect(result.foo.width).to.equal(40);
+    expect(result.foo.height).to.equal(100);
+    expect(result.foo.x).to.equal(0);
+    expect(result.foo.y).to.equal(0);
+
+    expect(result.bar.width).to.equal(60);
+    expect(result.bar.height).to.equal(75);
+    expect(result.bar.x).to.equal(40);
+    expect(result.bar.y).to.equal(0);
+
+    expect(result.baz.width).to.equal(60);
+    expect(result.baz.height).to.equal(25);
+    expect(result.baz.x).to.equal(40);
+    expect(result.baz.y).to.equal(75);
   });
 });
