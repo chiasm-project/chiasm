db3cd45829b02500747a41d8afb844f9dd64b945
curran <curran.kelleher@gmail.com>
2015-02-23 10:35:57 -0800

    Cleaned up scala code

--- scala/test.scala
+++ scala/test.scala
@@ -1,41 +1,48 @@
-// Load the data file
-//val data = sc.textFile("/Users/curran/repos/data/uci_ml/adult/adult.data")
-//val schema = sc.textFile("/Users/curran/repos/data/uci_ml/adult/schema.csv").collect()
+// This script is a proof of concept implementation
+// of data cube computation using Spark.
+// Curran Kelleher Feb 2015
+
+// Load the data files.
+// See https://github.com/curran/data/tree/gh-pages/uci_ml/adult
+val data = sc.textFile(  "/Users/Kelleher/repos/data/uci_ml/adult/data.csv")
+val schema = sc.textFile("/Users/Kelleher/repos/data/uci_ml/adult/schema.csv").collect()
 
 // Store the index of each attribute as a Map[String,Int].
-// Keys are attribute names, values are integer indices for looking
-// up values for the attribute in a given row of the table.
-val attributeIndices = schema.map(_.split(",")(0)).zipWithIndex.toMap
+// Keys are attribute names.
+// Values are integer indices for use with parsed CSV rows.
+val attributeIndices = schema.map(_.split(",")(0).trim).zipWithIndex.toMap
 
-case class Attribute(name: String, i: Integer = 0)
-
+// Define types for options arguments.
+// name is the attribute name.
+// i is the index of this attribute in rows.
+case class Dimension(name: String, i: Integer = 0)
 case class Measure(aggregationOp: String, name:String = "", i: Integer = 0)
 
-// Example dimensions, simulating a REST API argument.
-var dimensions = List(Attribute("sex"), Attribute("race"))
-//var dimensions = List(Attribute("sex"))
-
-//var measures = List(Measure("count"))
+// Example options, simulating REST API arguments.
+var dimensions = List(Dimension("sex"), Dimension("race"))
 var measures = List(Measure("count"), Measure("sum", "capital-gain"))
 
-// Add the index information for later use.
-dimensions = dimensions.map(d => Attribute(d.name, attributeIndices(d.name)))
+// Add attribute index information to dimensions and measures for later use.
+dimensions = dimensions.map(d => Dimension(d.name, attributeIndices(d.name)))
 measures = measures.map(m => 
   if (m.aggregationOp == "count"){
-    m
+    m // Index not necessary for "count"
   } else { 
     Measure(m.aggregationOp, m.name, attributeIndices(m.name))
   }
 )
 
-// Parse data into table, ignore rows not parsed correctly.
+// Parse data into table, ignoring rows not parsed correctly.
 val tupleSize = schema.length;
 val table = data.map(_.split(",").map(_.trim)).filter(_.length == tupleSize)
 
-// Computes a data cube of counts over the given dimensions.
-val result = table.map(row =>
+// Compute a data cube using the specified dimensions and measures.
+val cube = table.map(row =>
   (
+    // Each key is a List of dimension values.
     dimensions.map(d => row(d.i)),
+
+    // Each value is a List of measure values.
     measures.map(m =>
       if (m.aggregationOp == "count"){
         1.0
@@ -45,52 +52,23 @@ val result = table.map(row =>
     )
   )
 ).reduceByKey((a, b) =>
+
+  // Aggregate over unique dimension tuples
+  // by summing measure values.
   (a, b).zipped.map(_+_)
+
 ).collect()
-//val result = table.map(row =>
-//  (
-//    dimensions.map(d => row(d.i)), 
-//    measures.map(m =>
-//      if (m.aggregationOp == "count"){
-//        1.0
-//      } else if (m.aggregationOp == "sum"){
-//        row(m.i).toDouble
-//      }
-//    )
-//  )
-//).reduceByKey((a, b) => a.zip(b).map(_+_)).collect()
 
-println(result)
+println(cube)
 
-
-//// Compute the number of rows.
-//val tupleSize = data.first().split(",").length
-//
-//// Parse CSV rows.
-//// Filter out lines with parse errors.
-//val table = data.map(_.split(",").map(_.trim)).filter(_.length == tupleSize)
-//
-//
-//// Task 1: Detect columns with high cardinality.
-//// Task 2: Group by a single categorical dimension.
-//// Task 3: Group by numeric intervals (histogram).
-//// Task 3: Group by multiple categorical dimensions.
-//// Task 4: Group by multiple numeric intervals (2D histogram).
-//// Task 5: Group by numerical and categorical simultaneously.
-//
-//// Approaches:
-//// - Use Spark SQL, generate histogram bin columns
-//// - Use direct Spark, no need to store generated histogram bin assignments
-//
-//// This is one way of computing aggregations (multidimension group-by)
-////
-////val distinct1 = table.map(_(1)).distinct()
-////val distinct3 = table.map(_(3)).distinct()
-////
-////val product = distinct1.cartesian(distinct3).collect()
-////
-//////val result = distinct.zip(distinct.map(v => table.filter(_(1) == v).count()))
-////
-////val result = product.map(tuple => table.filter(_(1) == tuple(1)).count())
-////
-////result.foreach(println)
+// prints cube: Array[(List[String], List[Double])] = Array(
+// (List(Female, Other),List(109.0, 27759.0)), 
+// (List(Female, Amer-Indian-Eskimo),List(119.0, 64808.0)), 
+// (List(Female, Black),List(1555.0, 803303.0)), 
+// (List(Male, Asian-Pac-Islander),List(693.0, 1266675.0)), 
+// (List(Male, Amer-Indian-Eskimo),List(192.0, 129650.0)), 
+// (List(Female, White),List(8642.0, 4957141.0)), 
+// (List(Male, Black),List(1569.0, 1102151.0)), 
+// (List(Female, Asian-Pac-Islander),List(346.0, 269339.0)), 
+// (List(Male, White),List(19174.0, 2.6242964E7)), 
+// (List(Male, Other),List(162.0, 225534.0)))
