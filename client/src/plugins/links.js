2650d214504caab76602a2c096d00cb6f6925ccc
curran <curran.kelleher@gmail.com>
2015-03-31 12:09:00 -0700

    Added documentation, cleaned up code

--- client/src/plugins/links.js
+++ client/src/plugins/links.js
@@ -2,15 +2,17 @@
 // Created by Curran Kelleher Mar 2015
 define(["d3", "model"], function (d3, Model) {
 
-  return function CsvLoader(runtime) {
+  return function Links(runtime) {
 
     var model = Model({
-      publicProperties: [ "bindings" ],
-      numericColumns: []
+      publicProperties: [ "bindings" ]
     });
 
     model.when("bindings", function (bindings){
       bindings.forEach(function(bindingExpr){
+
+        // Parse the binding expression of the form
+        // "sourceAlias.sourceProperty -> targetAlias.targetProperty"
         var parts = bindingExpr.split("->").map(function(str){ return str.trim(); }),
             source = parts[0].split("."),
             sourceAlias = source[0],
@@ -18,10 +20,16 @@ define(["d3", "model"], function (d3, Model) {
             target = parts[1].split("."),
             targetAlias = target[0],
             targetProperty = target[1];
-        runtime.getComponent(sourceAlias, function(sourceComponent){
-          runtime.getComponent(targetAlias, function(targetComponent){
 
+        // Retreive the source and target components.
+        runtime.getComponent(sourceAlias, function(err, sourceComponent){
+          // TODO propagate errors to UI
+
+          runtime.getComponent(targetAlias, function(err, targetComponent){
+            // TODO propagate errors to UI
             // TODO keep track of listeners and remove old ones when bindings change.
+
+            // Add a reactive function that binds the source to the target.
             sourceComponent.when(sourceProperty, function(value){
               targetComponent[targetProperty] = value;
             });
