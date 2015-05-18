6618873ab83868f3684e3b48b3f1ba588825af14
J. Harshbarger <hypercubed@gmail.com>
2015-05-18 10:41:32 +0900

    Use internal path for plugins

--- kitchenSink/requireJSConfig.js
+++ kitchenSink/requireJSConfig.js
@@ -11,12 +11,10 @@
 
   // Here's how to can use a local development version
   // if this Gist is cloned into a sibling directory to the chiasm repo.
-  var chiasmPath = "../../chiasm/src/";
-  
-  var plugins = chiasmPath + "plugins/";
+  var chiasmPath = "../src/";
 
   requirejs.config({
-  
+
     // Set up paths for Bower dependencies.
     // Uses github.com/curran/cdn
     paths: {
@@ -24,38 +22,28 @@
       // Set up the Chiasm path.
       // https://github.com/curran/chiasm
       chiasm: chiasmPath + "chiasm",
-  
-      // Configure paths for Chiasm pluginss.
-      layout: plugins + "layout",
-      computeLayout: plugins + "computeLayout",
-      configEditor: plugins + "configEditor",
-      csvLoader: plugins + "csvLoader",
-      links: plugins + "links",
-      reactivis: plugins + "reactivis",
-      barChart: plugins + "barChart",
-      lineChart: plugins + "lineChart",
-      scatterPlot: plugins + "scatterPlot",
-  
+      "chiasm/plugins": chiasmPath + "plugins",
+
       // Visualization library.
       // http://d3js.org/
       d3: "//cdnjs.cloudflare.com/ajax/libs/d3/3.5.5/d3.min",
-  
+
       // Reactive model library.
       // https://github.com/curran/model
       model: "//curran.github.io/cdn/model-v0.2.1/dist/model",
-  
+
       // Functional programming utilities.
       // http://benmccormick.org/2014/11/12/underscore-vs-lodash/
       lodash: "//cdnjs.cloudflare.com/ajax/libs/lodash.js/3.5.0/lodash.min",
-  
+
       // Asynchronous control flow.
       // https://github.com/caolan/async
       async: "//cdnjs.cloudflare.com/ajax/libs/async/0.9.0/async",
-  
+
       // Syntax-highlighted text editor for code.
       // http://codemirror.net/
       codemirror: "//curran.github.io/cdn/codemirror-v5.0.0",
-  
+
       // Provides interactive color picker and slider for CodeMirror.
       // https://github.com/enjalot/Inlet.git
       inlet: "//curran.github.io/cdn/inlet/inlet",
