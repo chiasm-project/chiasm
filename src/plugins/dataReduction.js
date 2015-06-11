c8020f7b253736f6b904826f2275dfb4c50e9254
curran <curran.kelleher@gmail.com>
2015-06-11 14:21:47 -0700

    Added first pass at aggregation implementation

--- src/plugins/dataReduction.js
+++ src/plugins/dataReduction.js
@@ -1,42 +1,80 @@
-define(["d3"], function (d3){
-  return function (data){
-    return {
-      filter: function (predicates){
+define([], function (){
 
-				var result = data;
-				predicates.forEach(function (predicate){
-					if("min" in predicate){
-						result = result.filter(function (d){
-							return d[predicate.column] >= predicate.min;
-						});
-					}
-					if("max" in predicate){
-						result = result.filter(function (d){
-							return d[predicate.column] <= predicate.max;
-						});
-					}
-				});
-
-			  // The API is asynchronous, using Promises, in anticipation of
-				// server-side implementations.
-			  return Promise.resolve(result);
-      },
-      sample: function (options){
-        //options.n
-      },
-      aggregate: function (options){
-        options.cube.dimensions.forEach(function (dimension){
-          //dimension.columnName;
-          if(dimension.histogram){
-            //dimension.histogram.numBins
-            //dimension.histogram.niceBins // Boolean
-          }
-        });
-        options.cube.measures.forEach(function (measure){
-          //measure.columnName;
-          //measure.aggregationOp
+  function filter(data, predicates){
+    predicates.forEach(function (predicate){
+      if("min" in predicate){
+        data = data.filter(function (d){
+          return d[predicate.column] >= predicate.min;
         });
       }
-    };
+      if("max" in predicate){
+        data = data.filter(function (d){
+          return d[predicate.column] <= predicate.max;
+        });
+      }
+    });
+    return data;
+  }
+
+  function aggregate(data, options){
+
+    var dataByKey = {};
+
+    function getRow(d, dimensions){
+      var key = makeKey(d, dimensions);
+      if(key in dataByKey){
+        return dataByKey[key];
+      } else {
+        var row = makeRow(d, dimensions);
+        dataByKey[key] = row;
+        return row;
+      }
+    }
+
+    data.forEach(function (d){
+      var row = getRow(d, options.dimensions);
+      options.measures.forEach(function (measure){
+        var outColumn = measure.outColumn;
+        if(measure.operator === "count"){
+          row[outColumn] = (row[outColumn] || 0) + 1;
+        }
+      });
+    });
+
+    return Object.keys(dataByKey).map(function (key){
+      return dataByKey[key];
+    });
+  }
+
+  function makeKey(d, dimensions){
+    // TODO optimize
+    return dimensions.map(function (dimension){
+      return d[dimension.column];
+    }).join(";");
+  }
+
+  function makeRow(d, dimensions){
+
+    // TODO implement histogram dimensions
+    //if(dimension.histogram){
+    //  //dimension.histogram.numBins
+    //  //dimension.histogram.niceBins // Boolean
+    //}
+
+    var row = {};
+    dimensions.forEach(function (dimension){
+      row[dimension.column] = d[dimension.column];
+    });
+    return row;
+  }
+
+  return function (data, options){
+    if("filter" in options){
+      data = filter(data, options.filter);
+    }
+    if("aggregate" in options){
+      data = aggregate(data, options.aggregate);
+    }
+    return data;
   };
 });
