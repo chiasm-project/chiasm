b3e7ed8abcb1bb332236a5d7c0a0d0f557e047a1
curran <curran.kelleher@gmail.com>
2015-09-02 18:07:45 -0700

    Fix formatting

--- chiasm.js
+++ chiasm.js
@@ -1,10 +1,10 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Chiasm = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 (function (global){
-// Chiasm.js
+// chiasm.js
 // v0.2.0
 //
-// This file contains the core implementation of Chiasm, which is a runtime
-// environment and plugin architecture for interactive visualizations.
+// This file contains the core implementation of Chiasm, a runtime environment
+// and plugin architecture for interactive visualizations.
 //
 // The main purpose of this module is to maintain synchronization between a
 // dynamic JSON configuration structure and a set of components instantiated by
@@ -196,7 +196,8 @@ function Chiasm(){
 
     // `plugins` is An object for setting up plugins before loading a configuration.
     // Chiasm first looks here for plugins, then if a plugin is not found here
-    // it is dynamically loaded at runtime.
+    // it is dynamically loaded at runtime using RequireJS where the plugin name
+    // corresponds to an AMD module name or arbitrary URL.
     //
     // * Keys are plugin names.
     // * Values are plugin implementations, which are constructor functions for
@@ -533,6 +534,7 @@ function Chiasm(){
 Chiasm.configDiff = configDiff;
 Chiasm.Action = Action;
 
+// Return the Chiasm constructor function as this AMD module.
 module.exports = Chiasm;
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
