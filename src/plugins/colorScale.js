dfbfc89726e70020996ab21ada49054af7197aae
Curran <curran.kelleher@gmail.com>
2015-09-03 12:45:52 -0700

    Clear out gh-pages branch

--- src/plugins/colorScale.js
+++
@@ -1,9 +0,0 @@
-// A simple plugin that exposes a model for use in defining
-// a color scale shared across many visualizations.
-define(["model"], function(Model){
-  return function(){
-    return Model({
-      publicProperties: ["colorDomain", "colorRange"]
-    });
-  };
-});
